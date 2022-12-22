ffc12d32b7a1a1157b4772cfcf45f942a3a6f5de
Abdulrahman Alomar <alomardev@gmail.com>
2022-12-22 11:08:51 +0300

    clean

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
-
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const { runServer } = require('verdaccio');
+const PORT = 4880;
+runServer('config.yaml').then(app => {
+  console.log(`Listening to: http://localhost:${PORT}`);
+  app.listen(PORT);
+})
